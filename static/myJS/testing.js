4bcee78b97401676aed11021fe1b73a379f45ff5
manny94 <vazdeazm@uni.coventry.ac.uk>
2017-09-19 17:52:25 +0000

    last commit

---
+++ static/myJS/testing.js
@@ -0,0 +1,24 @@
+// JavaScript File
+
+var acc = document.getElementsByClassName("accordion");
+
+var i;
+
+for(i = 0; i < acc.length; i++) {
+    
+    acc[i].onClick = function(){
+        /*this.classList.toggle("active");*/
+        
+        var panel = this.nextElementSibling;
+        
+        if (panel.style.display === "block") {
+            
+            panel.style.display = "none";
+            
+        } 
+        
+        else {
+            panel.style.display = "block";
+        }
+    }
+}
