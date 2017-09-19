04e6205e5582b728077e0267adc84eb5fc662714
manny94 <vazdeazm@uni.coventry.ac.uk>
2017-09-19 16:57:44 +0000

    js, css linked to html & minor adjst to js code to hide and open content

---
+++ static/js/MyJS.js
@@ -0,0 +1,22 @@
+// JavaScript File
+
+var acc = document.getElementsByClassName("accordion");
+
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    
+    acc[i].onClick = function(){
+        this.classList.toggle("active");
+        
+        var panel = this.nextElementSibling;
+        
+        if (panel.style.maxHeight) {
+            
+            panel.style.maxHeight = null;
+            
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+}
